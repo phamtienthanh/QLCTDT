34e71e97b78a79e750305549308babccbdc5a752
Phạm Tiến Thành <thanhpt_560@vnu.edu.vn>
2013-05-21 22:46:59 +0700

    man hinh cho

---
+++ ATM/manhinhcho.cpp
@@ -0,0 +1,19 @@
+
+     cout<<"\n";
+     cout<<"\t   *    *    * "<<" ***** "<<" *    "<<"  **** "<<"  ****  "<<" *     * "<<" *****"<<endl;Sleep(100);
+     cout<<"\t   *    *    * "<<" *     "<<" *    "<<" *     "<<" *    * "<<" **   ** "<<" *    "<<endl;Sleep(100);
+     cout<<"\t   *   * *   * "<<" ***** "<<" *    "<<" *     "<<" *    * "<<" * * * * "<<" *****"<<endl;Sleep(100);
+     cout<<"\t    * *   * *  "<<" *     "<<" *    "<<" *     "<<" *    * "<<" *  *  * "<<" *    "<<endl;Sleep(100);
+     cout<<"\t     *     *   "<<" ***** "<<" *****"<<"  **** "<<"  ****  "<<" *  *  * "<<" *****"<<endl;Sleep(100);
+     cout<<"\n\n"<<endl;
+     cout<<"\t\t   **        **        **";Sleep(50);cout<<"    ***********   "<<endl;Sleep(50);
+     cout<<"\t\t   **        **        **";Sleep(50);cout<<"    ***      ***  "<<endl;Sleep(50);
+     cout<<"\t\t   **        **        **";Sleep(50);cout<<"    ***       **  "<<endl;Sleep(50);
+     cout<<"\t\t    **      ****      ** ";Sleep(50);cout<<"    ***********   "<<endl;Sleep(50);
+     cout<<"\t\t     **    **  **    **  ";Sleep(50);cout<<"    ***********   "<<endl;Sleep(50);
+     cout<<"\t\t      **  **    **  **   ";Sleep(50);cout<<"    ***       **  "<<endl;Sleep(50);
+     cout<<"\t\t       ****      ****    ";Sleep(50);cout<<"    ***      ***  "<<endl;Sleep(50);
+     cout<<"\t\t        **        **     ";Sleep(50);cout<<"    ***********   "<<endl;Sleep(50);
+     cout<<endl;
+     cout<<"\n  \t "<<(char)(5)<<"\tCHAO MUNG BAN DEN VOI HE THONG ATM WORLD BANK \t"<<(char)(5)<<endl;
+     cout<<"  \n\n\t\t  "<<(char)(4)<<"  Nhap phim 1 de THUC HIEN GIAO DICH:   ";
